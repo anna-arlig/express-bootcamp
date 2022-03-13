5bebfa205c3db2e4cae8278f2b86bb1525b1f23e
anna.arlig <you@example.com>
2022-03-13 13:58:59 +0100

    Add cookie-parser

--- cookie-parser/app.js
+++ cookie-parser/app.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+function cookieParser(req,res,next){
+    if(!req.headers.cookie){ next() }
+    else{
+        const cookieHeader = req.headers.cookie
+        let cookieStrings = cookieHeader.split("; ")
+        cookieStrings = cookieStrings.map(string => string.split('='))
+        const body = {}
+        for(let field of cookieStrings){
+            const [key, value] = [field[0], field[1]]
+            body[key] = value 
+          }
+        req.cookies = body
+        console.log(req.cookies)
+  
+        next()
+    }
+
+  }
+  
+  app.use(cookieParser)
+
 
 app.get('/', (req, res) =>{
-    res.set("Set-Cookie", "test="+"hej")
-    res.send("Hello")
-    
-    console.log(req.headers.cookie)
+
+  res.render('index', {test: req.cookies})
    
+})
+
+app.get('/about', (req, res) =>{
+    res.render('about', {test: req.cookies})
+  
 })
 
 
